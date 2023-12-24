749b42d2a04e4c186eb2a40f19c3fd27195efef7
Ankit <ankitagrawal1331@gmail.com>
2023-12-24 23:36:55 +0530

    google oauth implementation

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -32,3 +32,40 @@ export const signin = async (req, res, next) => {
     next(error);
   }
 };
+
+export const google = async (req, res, next) => {
+  try {
+    const validUser = await User.findOne({ email: req.body.email });
+    if (validUser) {
+      const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+      const { password: pass, ...rest } = validUser._doc;
+      res
+        .cookie("access_token", token, { httpOnly: true })
+        .status(200)
+        .json(rest);
+    } else {
+      const { name, email, photo } = req.body;
+      const generatedPassword =
+        Math.random().toString(36).split(-8) +
+        Math.random().toString(36).split(-8);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+      const newUser = new User({
+        username:
+          name.split(" ").join("").toLowerCase() +
+          Math.random().toString(36).split(-4),
+        email,
+        password: hashedPassword,
+        avatar: photo,
+      });
+      await newUser.save();
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const { password: pass, ...rest } = newUser._doc;
+      res
+        .cookie("access_token", token, { httpOnly: true })
+        .status(200)
+        .json(rest);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
